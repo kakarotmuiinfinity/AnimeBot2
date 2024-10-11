751fea862d0ad481b387ac909050d8854ed07c4a
kakarotmuiinfinity <166479138+kakarotmuiinfinity@users.noreply.github.com>
2024-10-11 05:58:51 +0530

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,4 @@
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+BOT_TOKEN = '7712183768:AAFB_5sDCNLNKcMwhvyjLP-nSFqkmSkiXkI'
+WELCOME_IMAGE_URL = 'https://te.legra.ph/file/d05ac856c4a8659de29ce.jpg'
+STICKER_ID = 'CAACAgUAAxkBAAIgL2cHg1wOoOZ7uBA5Q8uh8wF2DN1xAAIEAAPBJDExieUdbguzyBAeBA'
