3a88561dd103f7890046f92a4646aa68535dc981
kakarotmuiinfinity <166479138+kakarotmuiinfinity@users.noreply.github.com>
2024-10-11 07:31:18 +0530

    Create utils.py

---
+++ bot/utils.py
@@ -0,0 +1,5 @@
+# This file is for utility functions that might be used across different parts of the bot.
+# For now, it's empty, but you can add functions here as your bot grows.
+
+def example_utility_function():
+    pass
